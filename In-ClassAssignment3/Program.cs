08a908e7364c5cba2f545f058b4e2922e45f0c31
Ivan Rivas <IRivas1@my.gcu.edu>
2019-01-31 12:54:28 -0700

    New

--- In-ClassAssignment3/Program.cs
+++ In-ClassAssignment3/Program.cs
@@ -16,7 +16,7 @@ namespace In_ClassAssignment3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new HumanWeightToCancri55e());
         }
     }
 }
